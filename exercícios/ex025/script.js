1dbadb77025059d378fd707f5ad9509dfbe003d8
fernandoWillian888 <113860024+fernandoWillian888@users.noreply.github.com>
2023-08-17 21:44:54 -0300

    17.08.2023

---
+++ exercícios/ex025/script.js
@@ -0,0 +1,5 @@
+function cadastrar(){    
+    var nome = document.getElementById("idnome");
+    var sobrenome = document.getElementById("idsobrenome");
+    window.alert = `O nome e sobrenome do usuário é respectivamente: ${nome} ${sobrenome}`
+}
